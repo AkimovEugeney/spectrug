2bfff5e41e0bef83a0be6e98ca5de805a2bbba83
Eugene Akimov <akimovEugeney@gmail.com>
2024-02-05 20:40:28 +0300

    create advantages and consultation form

--- main.js
+++ main.js
@@ -15,3 +15,28 @@ const swiper = new Swiper('.swiper', {
   },
 
 });
+
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+     change.target.classList.add('anim');
+    }
+  });
+}
+
+let options = {
+  threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.cosultation__wrapp');
+console.log(elements)
+
+
+for (let elm of elements) {
+  observer.observe(elm);
+}
+
+// inputmask
+const inputs = document.querySelectorAll('input[type="tel"]');
+const im = new Inputmask('+7 (999) 999-99-99');
+im.mask(inputs)
